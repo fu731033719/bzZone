5e5baaf166af153dd9391ecfff7b8d0220593871
lot <15706804793@163.com>
2022-10-07 02:30:29 +0800

    article index

--- src/page/projectDetail/index.jsx
+++ src/page/projectDetail/index.jsx
@@ -3,33 +3,34 @@ import ReactMarkdown from 'react-markdown';
 import {
   useSearchParams,
 } from "react-router-dom";
+// import md1 from ;
 import './index.css';
-import md1 from '../../markdown/project-1/index.md';
 export default function ProjectDetail() {
     const [text, setText] = useState('');
     const [showMd, setShowMd] = useState(false);
     const [searchParams] = useSearchParams();
     const index = searchParams.get("index");
-    const onload = (path) => {
-      fetch(md1).then(res => {
-        /** finish load */
-        if(res.ok) {
-          return res.text(); 
-        } else {
-          alert(res)
-            throw new Error(res.statusText);
-        }
-      }).then(text => {
-        setText(text);
-        setShowMd(true);
+
+    const onload = () => {
+      import(`../../markdown/project-${index}/index.md`).then(md => {
+        fetch(md.default).then(res => {
+          /** finish load */
+          if(res.ok) {
+            return res.text(); 
+          } else {
+              throw new Error(res.statusText);
+          }
+        }).then(text => {
+          setText(text);
+          setShowMd(true);
+        })
+      }).catch(err => {
+        console.error('【ArticleError】：未发现对应文章');
       })
-      // import('../../markdown/project-1/index.md').then(url => {
-        
-      // });
     }
     useEffect(() => {
       onload();
-    }, []);
+    });
 
     return (
       <div style={{ padding: "1rem 0" }}>
