ac4d736b63ae8dee206c6242159d88e961bbab50
lot <15706804793@163.com>
2022-10-07 17:02:27 +0800

    add url

--- src/compontents/footer/index.jsx
+++ src/compontents/footer/index.jsx
@@ -3,6 +3,9 @@ import './index.css';
 
 import {Fragment} from 'react';
 function Footer() {
+  const openSocial = (url) => {
+    window.open(url);
+  }
   return (
     <Fragment>
         <div className='bottom'>
@@ -17,10 +20,10 @@ function Footer() {
                 <div className='social'>
                   <p>Follow Me</p>
                   <div className='social-icon'>
-                    <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/rebbook%20LOGO%402x.png" alt="" />
-                    <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/weibo%402x.png" alt="" />
-                    <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/instagram-logo%402x.png" alt="" />
-                    <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/BEHANCELOGO%402x.png" alt="" />
+                    <img onClick={e => openSocial('https://www.xiaohongshu.com/user/profile/5992445a50c4b46b02bac2ed?xhsshare=CopyLink&appuid=5992445a50c4b46b02bac2ed&apptime=1665126877')} src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/rebbook%20LOGO%402x.png" alt="" />
+                    <img onClick={e => openSocial('https://weibo.com/u/2868931234')} src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/weibo%402x.png" alt="" />
+                    <img onClick={e => openSocial('https://www.instagram.com/baozhu.carrie/')} src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/instagram-logo%402x.png" alt="" />
+                    <img onClick={e => openSocial('https://www.behance.net/yyulinsun-carrie')} src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/home/BEHANCELOGO%402x.png" alt="" />
                   </div>
                 </div>
               </div>
