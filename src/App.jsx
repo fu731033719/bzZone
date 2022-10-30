f37627f463cfab8700257427d594978eb46d633a
lot <15706804793@163.com>
2022-10-30 18:43:10 +0800

    style adjust

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ function App() {
           <div className="project-instance-list">
             <div className="project-item" onClick={e => locationProject()}>
               <div className='project-pic'>
+                <div className="project-pic-mask">
+                  <p>运营设计</p>
+                  <p>{`{多多奥运季}`}</p>
+                </div>
                 <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/project-cover/1.png" alt="" />
               </div>
               <p className='project-dec'>COMMERCIAL PROJECT</p>
@@ -30,6 +34,10 @@ function App() {
             </div>
             <div className="project-item" onClick={e => locationProject()}>
               <div className='project-pic'>
+                <div className="project-pic-mask">
+                  <p>系列插画</p>
+                  <p>{`{莫比乌斯MOBIUS}`}</p>
+                </div>
                 <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone/project-cover/7.png" alt="" />
               </div>
               <p className='project-dec'>CREATIVE  PROJECT</p>
@@ -41,17 +49,25 @@ function App() {
             </div>
             <div className="project-item" onClick={e => locationProject()}>
               <div className='project-pic'>
+                <div className="project-pic-mask">
+                  <p>ICON动效设计</p>
+                  <p>{`{PDD金刚区动效整理与总结}`}</p>
+                </div>
                 <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone%2Fproject-cover%2F2.png" alt="" />
               </div>
               <p className='project-dec'>COMMERCIAL PROJECT</p>
               <p className='project-dec'>首页ICON动效整理与总结</p>
               <div className='project-tag-list'>
                 <p className="project-tag">Pinduoduo</p>
-                <p className="project-tag">2021</p>
+                <p className="project-tag">2022</p>
               </div>
             </div>
             <div className="project-item" onClick={e => locationProject()}>
               <div className='project-pic'>
+                <div className="project-pic-mask">
+                  <p>潮流插画</p>
+                  <p>{`{FREE WILL}`}</p>
+                </div>
                 <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone%2Fproject-cover%2F8.png" alt="" />
               </div>
               <p className='project-dec'>CREATIVE  PROJECT</p>
@@ -63,6 +79,10 @@ function App() {
             </div>
             <div className="project-item" onClick={e => locationProject()}>
               <div className='project-pic'>
+                <div className="project-pic-mask">
+                  <p>品牌设计</p>
+                  <p>{`{RAVENSKILL 品牌设计}`}</p>
+                </div>
                 <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone%2Fproject-cover%2F3.png" alt="" />
               </div>
               <p className='project-dec'>COMMERCIAL PROJECT</p>
@@ -74,6 +94,10 @@ function App() {
             </div>
             <div className="project-item" onClick={e => locationProject()}>
               <div className='project-pic'>
+                <div className="project-pic-mask">
+                  <p>线下展览与活动</p>
+                  <p>{`{TED*UAL象堡没有堡}`}</p>
+                </div>
                 <img src="https://baozhucarrie-1305385933.cos.ap-shanghai.myqcloud.com/bzZone%2Fproject-cover%2F10.png" alt="" />
               </div>
               <p className='project-dec'>CREATIVE  PROJECT</p>
